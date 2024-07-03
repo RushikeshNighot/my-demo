dfd4fc692680e345b23eb14c9acd283fbfb5a636
RushikeshNighot <rushikeshnighot03@gmail.com>
2024-07-03 19:19:30 +0530

    intial commit

---
+++ arithmeticapplication/src/main/java/com/csi/core/AriithmaticApplication.java
@@ -0,0 +1,12 @@
+package com.csi.core;
+
+public class AriithmaticApplication {
+	public static void main(String[] args) {
+		int n1 = 2, n2 = 65, n3 = 55, result = 0;
+		result = n2 - n3;
+		System.out.println("\n Susbtraction result:" + result);
+		result = n1 - n3;
+		System.out.println("\n substration result:" + result);
+	}
+
+}
---
+++ arithmeticapplication/src/main/java/com/csi/core/ArithmeticApplication.java
@@ -0,0 +1,16 @@
+package com.csi.core;
+
+public class ArithmeticApplication {
+	public static void main(String[] args) {
+		int n1 = 454, n2 = 565, result = 0;
+		result = n2 - n1;
+		System.out.println("\n Susbstraction result :" + result);
+		result = n1 + n2;
+		System.out.println("\n Addition result :" + result);
+		result = n1 * n2;
+		System.out.println("\n Multiplication result:" + result);
+		result = n1 / n2;
+		System.out.println("\n Division result:" + result);
+	}
+
+}
